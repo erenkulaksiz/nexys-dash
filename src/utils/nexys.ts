fa8f2bf62bddfa6627a0aefaeb20a0f240307f70
Eren Kulaksiz <erenkulaksz@gmail.com>
2023-05-07 16:33:59 +0300

    feat: 0.9.0

--- src/utils/nexys.ts
+++ src/utils/nexys.ts
@@ -1,6 +1,6 @@
 import Nexys from "nexys";
 
-export const nexys = new Nexys("-6d80365a-ef22-47d6-8fbb-265c6ff6bf02", {
+export const nexys = new Nexys("6d80365a-ef22-47d6-8fbb-265c6ff6bf02", {
   appName: "nexys",
   debug: true,
   allowGeoLocation: false,
