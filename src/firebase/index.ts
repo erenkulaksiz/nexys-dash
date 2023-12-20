1f593ff7138761c1e25d7f45fb21a5c14281473e
Murat Kirazkaya <77299279+GroophyLifefor@users.noreply.github.com>
2023-12-20 23:05:16 +0300

    feat: Now, when the project is resurrected, instead of a complex error, we get a more problem-oriented error message and the project does not continue to resurrect.

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,7 +2,7 @@ import { initializeApp, getApps } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { isClient } from "@/utils";
 
-const firebaseConfig = {
+const firebaseConfig: FirebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
@@ -14,8 +14,20 @@ const firebaseConfig = {
 
 if (!getApps.length) {
   const app = initializeApp(firebaseConfig);
+
+  // I am checking for any missing configuration IDs.
+  (Object.keys(firebaseConfig) as Array<keyof FirebaseConfig>).forEach(key => {
+    const value = firebaseConfig[key];
+
+    if (!value) {
+      console.error(`Missing Firebase config value for "${key}"`);
+      throw new Error(`Missing Firebase config value for "${key}"`);
+    }
+  });
+
   if (isClient()) {
     if ("measurementId" in firebaseConfig) {
+      console.error(firebaseConfig);
       const analytics = getAnalytics(app);
     }
   }
